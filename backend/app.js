f50e0a59f26ac0d633d3439be84835bfd86c8980
Hippo <hippolytehamonet@galaxy-j4.home>
2021-06-01 14:50:07 +0200

    modif backend chemin images (mauvaise syntaxe qui renvoyait une erreur) et création middleware likes

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-
-
-
 const app = express();
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://hippo_83:wergeronbak1@cluster0.cty6h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,5 +23,9 @@ app.use((req, res, next) => {
   });
   app.use(bodyParser.json());
 
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  app.use('/api/sauces', sauceRoutes);
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userID = decodedToken.userID;
+        if(req.body.userID && req.body.userID !== userID){
+            throw 'user ID non valable';
+        }else {
+            next()
+        }
+    } catch (error) {
+        res.status(401).json({ error: error})
+    }
+
+};
